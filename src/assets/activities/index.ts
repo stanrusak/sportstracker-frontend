46f3ff97aeda3e68ab40e38ad3fb906b27d82a26
Stanislav Rusak <stanislav.rusak@gmail.com>
2023-07-25 18:45:53 +0300

    activity assets

---
+++ src/assets/activities/index.ts
@@ -0,0 +1,11 @@
+import weights from "./weight-training.jpeg";
+import flexibility from "./flexibility.jpeg";
+import climbing from "./climbing.jpeg";
+import badminton from "./badminton.jpeg";
+
+export default {
+  "Weight Training": weights,
+  Flexibility: flexibility,
+  Climbing: climbing,
+  Badminton: badminton,
+};
