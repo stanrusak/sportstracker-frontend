4f68e3f3f00577015b8921920e2de5f8286d26aa
Stanislav Rusak <stanislav.rusak@gmail.com>
2023-07-30 14:35:53 +0300

    implemented sidebar and page switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
+import { useEffect, useState } from "react";
 import { Navbar } from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
-import { useAuth } from "./utils/auth";
 import Dashboard from "./pages/Dashboard";
+import { useAuth } from "./utils/auth";
 
+import Sidebar from "./components/sidebar/Sidebar";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const App = () => {
-  const { token } = useAuth();
+  const { token, setUserData } = useAuth();
+  const [activePage, setActivePage] = useState("Dashboard");
+
+  useEffect(() => {
+    setUserData({ activities: [1, 2], exercises: [1, 3, 4] });
+  }, []);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <div className="h-screen w-screen overflow-x-hidden ">
+      <div className="h-screen w-screen overflow-y-auto bg-bgprimary-default text-primary">
         <Navbar />
-        {token ? <Dashboard /> : <LandingPage />}
+        {token ? (
+          <div className="flex h-full w-full overflow-x-hidden pt-navbar">
+            <Sidebar activePage={activePage} setActivePage={setActivePage} />
+            <div className="ml-6 flex h-full flex-col px-[100px]">
+              <h1 className="mt-6 text-4xl font-extrabold leading-relaxed">
+                {activePage}
+              </h1>
+              <div className="flex gap-4">
+                {activePage === "Dashboard" && <Dashboard />}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <LandingPage />
+        )}
       </div>
     </LocalizationProvider>
   );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,14 @@
+import { useState } from "react";
 import Activities from "../components/activities/Activities";
 import Exercises from "../components/exercises/Exercises";
 import Sessions from "../components/sessions/Sessions";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col bg-bglight px-[100px] pt-[80px]">
-      <h1 className="text-3xl font-extrabold leading-relaxed">Dashboard</h1>
-      <div className="flex gap-4">
-        <Sessions />
-        <Activities />
-      </div>
+    <div className="flex gap-10">
+      <Sessions />
+      <Activities />
       <Exercises />
     </div>
   );
